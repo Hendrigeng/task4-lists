127bdf2fd7363ce9639b5966ed7a8b360d47e5f8
hend <hendrigeng13@gmail.com>
2023-12-25 06:01:58 +0200

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,24 @@ void main()
   var car1=Car('pegout', '3008', 2015);
   car1.printinfo();
   print("//////////////////");
+
+
   var smallcar2=Samllcar('pegout', '3008', 2015, 4);
   smallcar2.printinfo();
   print("//////////////////");
+
+
   var familycar2=Familycar('pegout', '3008', 2015);
   familycar2.printinfo();
   print(familycar2.brand);
   print("//////////////////");
+
+
  var  servise2=Service('pegout', '3008', 2015);
  servise2.printinfo();
   print("//////////////////");
+
+
   var moto2= Motor('pegout', '3008', 2015, 135446);
   moto2.printinfo();
 
